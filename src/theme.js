66bbe7d830fb97cd5312e9b733719f98ad5585af
Bright <iortsor@gmail.com>
2023-01-04 18:49:36 +0100

    initialized repository

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/system";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1760a5",
+      light: "#4f8cd8",
+    },
+    secondary: {
+      main: "#f9a825",
+      light: "#fcd46d",
+    },
+    otherColor: {
+      main: "#999",
+    },
+  },
+});
