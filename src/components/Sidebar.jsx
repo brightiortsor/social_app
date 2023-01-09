f15132cbea2550f9696f396dc80485d4bf813478
Bright <iortsor@gmail.com>
2023-01-09 19:12:17 +0100

    ADDED/sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,27 @@
-import { Box } from "@mui/material";
+import { Home } from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      Sidebarrrs
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="/home">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
