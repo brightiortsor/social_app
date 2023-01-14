74627ccbb3b3747674eaded98d6f06c740d9a857
Bright <iortsor@gmail.com>
2023-01-14 17:01:29 +0100

    ADDED/dark mode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import Navbar from "./components/Navbar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
-import { Box } from "@mui/material";
-import { Stack } from "@mui/system";
+import { Box, createTheme, ThemeProvider, Stack } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-      <Add />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,14 +1,114 @@
-import { Fab, Tooltip } from "@mui/material";
-import { Add as AddIcon } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import {
+  Add as AddIcon,
+  Call,
+  Image,
+  EmojiEmotions,
+  PersonAdd,
+  VideoCameraBack,
+  DateRange,
+} from "@mui/icons-material";
+import { useState } from "react";
+import styled from "@emotion/styled";
+import { Stack } from "@mui/system";
+
+const StyledModal = styled(Modal)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: 10,
+  marginBottom: 20,
+}));
 
 const Add = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <Tooltip title="Add Post">
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Add Post"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
         <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
       </Tooltip>
+
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          width={400}
+          height={280}
+          bgcolor={"background.default"}
+          color={"text.primary"}
+          borderRadius={5}
+          p={3}
+        >
+          <Typography variant="h6" color="gray" textAlign="center">
+            Add Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://randomuser.me/api/portraits/men/68.jpg"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography variant="span" fontWeight={500}>
+              Remy Sharp
+            </Typography>
+          </UserBox>
+
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={4}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="error" />
+            <Image color="primary" />
+            <Call color="success" />
+            <VideoCameraBack color="secondary" />
+            <PersonAdd color="success" />
+          </Stack>
+
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+            fullWidth
+          >
+            <Button sx={{ width: "200px", marginRight: "2px" }}>
+              Post Later <DateRange />
+            </Button>
+            <Button sx={{ width: "200px", marginLeft: "2px" }}>Post Now</Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
     </>
   );
 };
